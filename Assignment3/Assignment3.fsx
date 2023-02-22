035da555bfa3ef0aa1615b3c8fba4fc2cf016eb5
oljh <oljh@itu.dk>
2023-02-22 08:15:31 +0100

    idk

--- Assignment3/Assignment3.fsx
+++ Assignment3/Assignment3.fsx
@@ -2,5 +2,7 @@
     | Leaf
     | Node of BinTree<'a> * 'a * BinTree<'a>;;
 
+
+
 let fromList = List.fold (fun acc elem -> insert elem acc) Leaf lst;;
 let fromList2 lst = List.foldBack insert lst Leaf
