dd594eca9bbb756a8b49c09f079c196404ae29fa
OliFrys <oljh@itu.dk>
2023-03-01 17:29:08 +0100

    Assigment5 started

---
+++ Assignment5/Program.fs
@@ -0,0 +1,20 @@
+﻿// For more information see https://aka.ms/fsharp-console-apps
+let sum m n = 
+    let rec sumA acc = 
+        function
+        | 0 -> acc + m
+        | n' -> sumA (n' + m + acc) (n'-1)
+    sumA 0 n
+
+let length lst = 
+    let rec lengthA acc =
+        function
+        | [] -> acc
+        | x::xs -> lengthA (acc + 1) xs
+    lengthA 0 lst
+
+let foldBack f lst acc =
+    let rec foldBacKC f lst acc c =
+        match lst with
+        | [] -> c acc
+        | x::xs -> foldBackC f xs 
