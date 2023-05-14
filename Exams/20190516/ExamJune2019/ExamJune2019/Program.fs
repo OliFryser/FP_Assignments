7410b18298a21bda194dc28a68d699be25bd16bc
OliFrys <oljh@itu.dk>
2023-05-14 23:58:31 +0200

    Q1 done

--- Exams/20190516/ExamJune2019/ExamJune2019/Program.fs
+++ Exams/20190516/ExamJune2019/ExamJune2019/Program.fs
@@ -3,19 +3,19 @@
 | S of Peano
 
 // Q1.1
-let toInt =
+let toInt : Peano -> uint32 =
     let rec aux acc =
         function
         | O -> acc
-        | S p -> aux (acc + 1) p
+        | S p -> aux (acc + 1u) p
 
-    aux 0
+    aux 0u
 
 let fromInt =
     let rec aux acc =
         function
-        | 0 -> acc
-        | n -> aux (S acc) (n-1)
+        | 0u -> acc
+        | n -> aux (S acc) (n-1u)
 
     aux O
 
@@ -29,15 +29,59 @@ let rec mult p1 p2 =
     match p1 with
     | O -> O
     | S O -> p2 
-    | S p' -> mult p' (add p2 p2)
+    | S p' -> add p2 (mult p' p2)
 
 let rec pow p1 p2 =
     match p2 with
     | O -> S O
-    | S O -> p2 
-    | S p' -> pow (mult p1 p1) p'
+    | S O -> p1
+    | S p' -> mult p1 (pow p1 p')
 
 
+// Q1.3
+let tailAdd a (b : Peano) =
+    let rec aux a' acc =
+        match a' with
+        | O -> acc
+        | S p' -> aux p' (S acc)
+    
+    aux a b
+
+let tailMult a b =
+    let rec aux a' acc =
+        match a' with
+        | O -> O
+        | S O -> tailAdd b acc
+        | S p' -> aux p' (tailAdd b acc)
+
+    aux a O
+  
+let tailPow a b =
+    let rec aux b' acc =
+        match b' with
+        | O -> S O
+        | S O -> acc
+        | S p' -> aux p' (tailMult a acc)
+
+    aux b a
+
+
+// Q1.4
+let rec loop (f : ('a -> 'a)) (acc : 'a) (p : Peano) : 'a =
+   match p with
+   | O -> acc
+   | S p' -> loop f (f acc) p'
+   
+// Q1.5
+let loopAdd a b =
+    loop (fun p -> S p) a b
+
+let loopMult a b =
+    loop (fun p -> loopAdd p b) O a 
+
+let loopPow a b =
+    if b = O then S O 
+    else loop (fun p -> loopMult p a) (S O) b
 
 // Quesiton 2
 
